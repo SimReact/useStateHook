4ca6ba2760d6638c890143569959d3cca4e41d3c
Simon Duquaine <66021506+simonduquaine@users.noreply.github.com>
2020-11-06 10:17:27 +0100

    app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 let count = 0;
+
+function increase() {}
 
 ReactDOM.render(
   <div className="container">
