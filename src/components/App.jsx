a9cf92971f7747efa345b8ed621b2136ecc01090
Simon Duquaine <66021506+simonduquaine@users.noreply.github.com>
2020-11-06 10:25:20 +0100

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return <div />;
+  let count = 0;
+
+  function increase() {
+    count++;
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-let count = 0;
-
-function increase() {}
-
-ReactDOM.render(
-  <div className="container">
-    <h1>{count}</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
